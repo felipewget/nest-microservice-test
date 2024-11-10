bb1d03d2a01d8c7a317995939af5e3e794d0e458
Felipe Oliveira <felipe.wget@gmail.com>
2024-11-10 20:52:32 +1000

    Notification service with GAPI

---
+++ apps/notifications/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
+import { NotificationsModule } from './notifications.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(NotificationsModule);
+  const configService = app.get(ConfigService);
+
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get<number>('PORT'),
+    },
+  });
+  await app.startAllMicroservices();
+}
+bootstrap();
